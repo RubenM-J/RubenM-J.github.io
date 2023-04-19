05100cdf4e3440b51612043446ffc8d59ff86d6e
Ruben <43784210+RubenM-J@users.noreply.github.com>
2023-04-19 13:14:59 +0200

    Added year calculation

--- js/site.js
+++ js/site.js
@@ -36,3 +36,9 @@ $(".progress-bar").each(function () {
     each_bar_width = $(this).attr('aria-valuenow');
     $(this).width(each_bar_width + '%');
 });
+
+function getYear() {
+    var today = new Date();
+    var yyyy = today.getFullYear();
+    document.getElementById("copyright").innerHTML = "&copy;" + yyyy + " - Pulsar Project"
+};
